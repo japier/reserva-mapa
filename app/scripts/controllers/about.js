1d1ca861d60e2ba63453457d73a5f52f0d6b0236
Javier Castro <jcastro@ooyala.com>
2015-04-05 21:24:33 -0500

    first

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thtReservaMapaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the thtReservaMapaApp
+ */
+angular.module('thtReservaMapaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thtReservaMapaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thtReservaMapaApp
+ */
+angular.module('thtReservaMapaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
